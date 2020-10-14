600457b78e024f5fd5840fd68bfd78901f5230e2
ahmada3mar <ahmada3mar@gmail.com>
2020-10-15 00:00:23 +0300

    14-10-2020

---
+++ 14-10-2020.js
@@ -0,0 +1,25 @@
+var arr = [];
+var even = 0 ;
+var odd = 0 ;
+
+function Myfunction(){
+
+    for (i = 1 ; i <= 10 ; i++ ) {
+
+        if (i % 2 === 0 ){
+            even += i ;
+
+        }else{
+            odd += i ;
+        }
+
+        arr.push(i);
+
+    }
+
+    alert("The sum of even number is : " + even);
+    alert("The sum of odd number is : " + odd);
+    alert("The array is : " + arr);
+}
+
+Myfunction();
